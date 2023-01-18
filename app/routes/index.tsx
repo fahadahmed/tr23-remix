b1c5e86f8b9e8ad980e740fd2b50943f9782d648
Fahad Ahmed <fahad.ahmed@me.com>
2023-01-18 13:48:43 +1100

    code formatting

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -48,7 +48,9 @@ export default function Index() {
   const { todos } = useLoaderData();
   const submit = useSubmit();
   let transition = useTransition();
-  let isAdding = transition.state === 'submitting' && transition.submission.formData.get('_action') === 'create';
+  let isAdding = 
+    transition.state === 'submitting' && 
+    transition.submission.formData.get('_action') === 'create';
   let formRef: MutableRefObject<any> = useRef();
 
   const handleChange = (e: React.BaseSyntheticEvent) => {
@@ -65,13 +67,22 @@ export default function Index() {
     <div className="app-container">
       <h1 className="title">Transformation Realised - 2023</h1>
       {todos.length === 0 ? (
-        <p className="empty-message">There are no todos create yet. Add some using the form below.</p>
+        <p className="empty-message">
+          There are no todos create yet. Add some using the form below.
+        </p>
       ) : null}
       {todos.map((task: Task) => {
         return (
           <div key={task.id} className="todo-container">
             <Form method='post' onChange={handleChange}>
-              <input type="checkbox" aria-label="complete task" name="toggle" value={task.status} onChange={() => {}} checked={task.status === 'complete'} />
+              <input 
+                type="checkbox" 
+                aria-label="complete task" 
+                name="toggle"
+                value={task.status} 
+                onChange={() => {}} 
+                checked={task.status === 'complete'} 
+              />
               <input type="hidden" name="task" value={JSON.stringify(task)} />
               <input type="hidden" name="_action" value="status" />
             </Form>
@@ -82,14 +93,27 @@ export default function Index() {
               }</span>
             <Form method="post">
               <input type="hidden" name="task" value={JSON.stringify(task)} />
-              <button type="submit" name="_action" value="delete" className="delete-button">x</button>
+              <button 
+                type="submit" 
+                name="_action" 
+                value="delete" 
+                className="delete-button"
+              >
+                x
+              </button>
             </Form>
           </div>
         );
       })}
       <Form ref={formRef} method='post' className="create-task-container">
         <input type="text" name="task" placeholder="What do you want to do?" />
-        <button type="submit" name="_action" value="create">{isAdding ? 'Creating...' : 'Create task'}</button>
+        <button 
+          type="submit" 
+          name="_action" 
+          value="create"
+        >
+          {isAdding ? 'Creating...' : 'Create task'}
+        </button>
       </Form>
     </div>
   );
